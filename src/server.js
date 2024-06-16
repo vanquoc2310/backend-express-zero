4a8614c1c0d526e124484d4d49ed638884d96963
vanquoc2310 <namhuy041@gmail.com>
2024-06-17 01:45:30 +0700

    upload controller & service)

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const express = require('express')
 const configViewEngine = require('./config/viewEngine');
-const authRoutes = require('./routes/auth.js');
+const webRoutes = require('./routes/web.js');
 const sequelize = require('./models');
 
 
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended: true})); // for form data
 configViewEngine(app);
 
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', webRoutes);
 
 // const path = require('path');
 
