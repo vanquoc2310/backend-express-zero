a303f22b07845e242b285fd90ca6aa2bbbd022d5
vanquoc2310 <namhuy041@gmail.com>
2024-07-10 15:33:20 +0700

    upload logout + feedback

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { login, register, verifyEmail, requestPasswordReset, resetPassword  } = require('../controllers/authController');
+const { login, register, verifyEmail, requestPasswordReset, resetPassword, logout  } = require('../controllers/authController');
 
 
 // Route xử lý đăng nhập
@@ -10,6 +10,7 @@ router.get('/verify/:userId', verifyEmail);
 router.post('/request-password-reset', requestPasswordReset);
 //router.get('/reset-password', showResetPasswordForm); // Trang form để nhập mật khẩu mới
 router.post('/reset-password', resetPassword);
+router.post('/logout', logout)
 
 
 module.exports = router;
